d7a50ef01f98004b864bcf85909b283c15eb657e
Jonathan LEGRAND <jlegrand62@gmail.com>
2025-01-27 14:36:10 +0100

    Remove pkglts configuration and metadata files
    This commit deletes all files related to `pkglts`, including configuration, metadata, and generated files.
    The project no longer relies on `pkglts` for setup or management, simplifying the repository.

--- HISTORY.rst
+++
@@ -1,8 +0,0 @@
-=======
-History
-=======
-
-creation (2019-11-21)
-------------------------
-
-* First release on PyPI.
--- src/spectral_clustering/__init__.py
+++ src/spectral_clustering/__init__.py
@@ -1,10 +1,3 @@
 """
 A Python package to perform segmentation of 3D plant point clouds.
 """
-# {# pkglts, base
-
-from . import version
-
-__version__ = version.__version__
-
-# #}
